ee87f64a922eba4af488838ea7f091ab9f5c1a62
Florian Scheugenpflug <florian.scheugenpflug@kraftwerk.io>
2026-03-18 02:35:43 +0100

    fix(proxy): add structured pino logging to mitmserver

--- mitmserver/server.js
+++ mitmserver/server.js
@@ -1,41 +1,39 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
+const pino = require('pino');
 
+const logger = pino();
 const proxy = httpProxy.createProxyServer({});
 
 const server = http.createServer((req, res) => {
-  console.log(JSON.stringify({
-    level: 'info',
+  logger.info({
     msg: 'Request received',
     method: req.method,
     url: req.url,
     headers: req.headers
-  }));
+  });
 
   proxy.web(req, res, { target: 'http://localhost:3000' }, (e) => {
-    console.error(JSON.stringify({
-      level: 'error',
+    logger.error({
       msg: 'Proxy error',
       error: e.message || String(e)
-    }));
+    });
     res.writeHead(502);
     res.end('There was an error proxying the request.');
   });
 });
 
 proxy.on('proxyRes', (proxyRes, req, res) => {
-  console.log(JSON.stringify({
-    level: 'info',
+  logger.info({
     msg: 'Response received',
     status: proxyRes.statusCode,
     headers: proxyRes.headers
-  }));
+  });
 });
 
 const PORT = 8080;
 server.listen(PORT, () => {
-  console.log(JSON.stringify({
-    level: 'info',
+  logger.info({
     msg: `MITM Proxy server listening on port ${PORT}`
-  }));
+  });
 });
